264a9202c133b379410958514ae21ae206efd4e6
Ted <ted.eriksson@conjure.co.uk>
2015-01-06 14:03:49 +0000

    add facebook logging

--- app/src/main/java/uk/co/conjure/androidtesting/MainActivity.java
+++ app/src/main/java/uk/co/conjure/androidtesting/MainActivity.java
@@ -1,9 +1,24 @@
 package uk.co.conjure.androidtesting;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AppEventsLogger;
+import com.facebook.Session;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +27,34 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logEventButton = (Button) findViewById(R.id.logEvent);
+
+        logEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AppEventsLogger logger = AppEventsLogger.newLogger(MainActivity.this,(Session) null);
+                logger.logEvent("Log Event Button Pressed");
+
+                Log.d("FB", "Logged Event");
+                Toast.makeText(MainActivity.this, "Logged Event", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        AppEventsLogger.activateApp(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
